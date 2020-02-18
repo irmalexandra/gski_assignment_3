469e97b7c09f5e1a4df05eb7c468af5d5c657e53
lokialex <lokih@ru.is>
2020-02-18 10:30:02 +0000

    derp

---
+++ dll.py
@@ -0,0 +1,38 @@
+
+class Node:
+    pass
+
+class DLL:
+    def __init__(self):
+        self.current_position = None
+        self.size
+
+    def __len__(self):
+        pass
+
+    def insert(self, value):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_value(self):
+        pass
+
+    def move_to_next(self):
+        pass
+
+    def move_to_prev(self):
+        pass
+
+    def move_to_pos(self, position):
+        pass
+
+    def remove_all(value):
+        pass
+
+    def reverse(self):
+        pass
+
+    def sort():
+        pass
